d2bbfb8130bb0eb9fec1ea7655047a6d6d4b9b30
angga <anggashin@gmail.com>
2026-01-05 23:04:03 +0800

    feat: Add Opcodes Log Viewer package with configuration, assets, and UI integration.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -35,6 +35,14 @@
                         <span class="ml-2 text-sm tracking-wide truncate">Messages</span>
                     </a>
                 </li>
+                <li>
+                    <a href="{{ url('log-viewer') }}" target="_blank" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-700 text-gray-100 border-l-4 border-transparent hover:border-blue-500 pr-6 {{ request()->is('log-viewer*') ? 'bg-gray-700 border-blue-500' : '' }}">
+                        <span class="inline-flex justify-center items-center ml-4">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                        </span>
+                        <span class="ml-2 text-sm tracking-wide truncate">System Logs</span>
+                    </a>
+                </li>
                 <li class="px-5">
                     <div class="flex flex-row items-center h-8">
                         <div class="text-sm font-light tracking-wide text-gray-400 uppercase">Settings</div>
